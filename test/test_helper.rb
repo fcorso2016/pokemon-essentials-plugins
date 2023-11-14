f6b8507376eecae190569aab4c29bece359b5c8f
Francesco Corso <fcorso2016@gmail.com>
2023-11-14 17:32:08 -0500

    Moved some files around in the snag list

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,17 +5,11 @@ unless $disable_tests
   require 'minitest/autorun' # Sets up minitest
   require 'spy'
 
-  require_relative '../../../Export/rgss_mocks'
-  require_relative '../../../Export/pokemon_essentials'
+  require 'mkxp-z'
+  MKXP_Z.init_game_state('Pokemon Essentials', [], false)
 
-  require_relative '../src/battle/battle'
-  require_relative '../src/data/pokemon_global_metadata'
-  require_relative '../src/data/seen_shadow_pokemon'
-  require_relative '../src/data/trainer'
-  require_relative '../src/ui/menu_handlers'
-  require_relative '../src/ui/pokemon_snag_list'
-  require_relative '../src/ui/pokemon_snag_list_scene'
-  require_relative '../src/ui/window_snag_list'
+  require_relative '../../../Export/pokemon_essentials'
+  require_relative '../lib/snag_list'
 
   require_relative 'mocks'
 end
