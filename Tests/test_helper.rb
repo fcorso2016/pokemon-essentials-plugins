66ab20dc45041bedc172c64ed61022321cf31ae1
Francesco Corso <fcorso2016@gmail.com>
2023-11-18 20:58:08 -0500

    Specify the coverage file correctly

--- Tests/test_helper.rb
+++ Tests/test_helper.rb
@@ -1,6 +1,9 @@
 require 'simplecov' # These two lines must go first
 SimpleCov.start
 
+require "simplecov_json_formatter"
+SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
+
 require 'minitest/autorun' # Sets up minitest
 require 'spy'
 
