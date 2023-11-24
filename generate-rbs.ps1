72a318cf469bcdafd2aedfd310bdfb8c8138c83e
Francesco Corso <fcorso2016@gmail.com>
2023-11-23 21:50:58 -0500

    Started making RBS files for Essentials

---
+++ generate-rbs.ps1
@@ -0,0 +1,12 @@
+Set-Location Data/Scripts
+Get-ChildItem -Path . -Recurse -File | Resolve-Path -Relative | ForEach-Object {
+    $targetPath = "../../sig/Data/Scripts/${_}s"
+    Write-Output "Processing file: ${_}"
+    if (-NOT (Test-Path $targetPath)) {
+        $directory = Split-Path -Parent $targetPath
+        New-Item -ItemType Directory -Force -Path $directory
+        rbs prototype rb "$_" >> "$targetPath"
+        #typeprof "$targetPath" "$_" -I . >> "$targetPath"
+    }
+}
+Set-Location ../..
