a0d9f6befb915e619b65b588132f9aa3ce088934
Francesco Corso <fcorso2016@gmail.com>
2023-11-24 13:15:08 -0500

    Created defs for the entire technical folder

--- generate-rbs.ps1
+++ generate-rbs.ps1
@@ -1,12 +1,12 @@
 Set-Location Data/Scripts
 Get-ChildItem -Path . -Recurse -File | Resolve-Path -Relative | ForEach-Object {
-    $targetPath = "../../sig/Data/Scripts/${_}s"
+    $targetPath = "../../temp/Data/Scripts/${_}s"
     Write-Output "Processing file: ${_}"
     if (-NOT (Test-Path $targetPath)) {
         $directory = Split-Path -Parent $targetPath
         New-Item -ItemType Directory -Force -Path $directory
-        #rbs prototype rbi "$_" >> "$targetPath"
-        typeprof "$targetPath" "$_" -I . >> "$targetPath"
+        rbs prototype rbi "$_" >> "$targetPath"
+        #typeprof "$targetPath" "$_" -I . >> "$targetPath"
     }
 }
 Set-Location ../..
