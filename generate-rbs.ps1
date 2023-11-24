938ed35e8dfc4bd556ec8fc2453edf39f36de876
Francesco Corso <fcorso2016@gmail.com>
2023-11-24 11:38:22 -0500

    Reformatted a bunch of files to use typeprof instead

--- generate-rbs.ps1
+++ generate-rbs.ps1
@@ -1,11 +1,12 @@
-Set-Location Plugins
+Set-Location Data/Scripts
 Get-ChildItem -Path . -Recurse -File | Resolve-Path -Relative | ForEach-Object {
-    $targetPath = "../sig/Plugins/${_}s"
+    $targetPath = "../../sig/Data/Scripts/${_}s"
     Write-Output "Processing file: ${_}"
     if (-NOT (Test-Path $targetPath)) {
         $directory = Split-Path -Parent $targetPath
         New-Item -ItemType Directory -Force -Path $directory
-        rbs prototype rb "$_" >> "$targetPath"
+        #rbs prototype rbi "$_" >> "$targetPath"
+        typeprof "$targetPath" "$_" -I . >> "$targetPath"
     }
 }
-Set-Location ..
+Set-Location ../..
