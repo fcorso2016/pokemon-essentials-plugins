e4bb8d451253b1e9b4cc5f9021b36252018ec46d
fcorso2016 <35567462+fcorso2016@users.noreply.github.com>
2023-12-26 19:39:41 -0500

    Changed the code report to GitHub's SARIF format

--- type_reporter.rb
+++ type_reporter.rb
@@ -1,29 +1,47 @@
 require 'json'
-require 'digest'
 
+$rules = {}
 $issues = []
 
 def process_steep_report(report_contents)
   current_issue = {}
   report_contents.each_line do |line|
-    if line =~ /((?:[\w\s]+\/)+[\w\s]+\.(?:rb|rbs)):(\d+):\d+: \[(\w+)\] (.*)/i
+    if line =~ /((?:[\w\s]+\/)+[\w\s]+\.(?:rb|rbs)):(\d+):(\d+): \[(\w+)\] (.*)/i
       unless current_issue.empty? && !$issues.any? { |issue| issue == current_issue }
         yield current_issue
       end
       current_issue = {
-        "description" => $4.to_s,
-        "check_name" => "Unknown Error Type",
-        "fingerprint" => Digest::MD5.hexdigest(line),
-        "severity" => $3.to_s,
-        "location" => {
-          "path" => $1.to_s,
-          "lines" => {
-            "begin" => $2.to_i
-          }
-        }
+        "ruleId" => "RBS Type Error",
+        "level" => $4.to_s,
+        "message" => {
+          "text" => $5.to_s
+        },
+        "locations" => [{
+                          "physicalLocation" => {
+                            "artifactLocation" => {
+                              "uri" => $1.to_s,
+                              "uriBaseId" => "%SRCROOT%"
+                            },
+                            "region" => {
+                              "startLine" => $2.to_i,
+                              "startColumn" => $3.to_i,
+                              "endLine" => $2.to_i,
+                              "endColumn" => $3.to_i
+                            }
+                          }
+                        }]
       }
     elsif !current_issue.empty? && line =~ /Diagnostic ID: (.*)/i
-      current_issue["check_name"] = $1.to_s
+      current_issue["ruleId"] = $1.to_s
+      $rules[current_issue["ruleId"]] ||= {
+        "id" => current_issue["ruleId"],
+        "shortDescription" => {
+          "text" => "RBS Type Scan Issue"
+        },
+        "properties" => {
+          "tags" => %w[ruby type-error]
+        }
+      }
     end
   end
 end
@@ -35,6 +53,22 @@ if File.exist?("rbs_report.txt")
   end
 
   File.open("rbs_issues.json", "w") do |output|
-    output.write($issues.to_json)
+    full_json = {
+      "$schema" => "http://json.schemastore.org/sarif-2.1.0",
+      "version" => "2.1.0",
+      "runs" => [
+        {
+          "tool" => {
+            "driver" => {
+              "name" => "Steep Type Checker",
+              "version" => `steep --version`.strip,
+              "rules" => $rules.values
+            },
+          },
+          "results" => $issues
+        }
+      ]
+    }
+    output.write(full_json.to_json)
   end
 end
