ab16d96e143b11182e7a32855ec8f6578734d71c
fcorso2016 <35567462+fcorso2016@users.noreply.github.com>
2023-12-28 16:54:02 -0500

    Added test to handle the creation of the snag list window

--- Tests/snag_list/window_snag_list_test.rb
+++ Tests/snag_list/window_snag_list_test.rb
@@ -1,17 +1,61 @@
 # frozen_string_literal: true
 
-require 'minitest/autorun'
+require_relative '../test_helper'
+
+$drawn_text = []
+
+def pbDrawShadowText(bitmap, x, y, width, height, string, baseColor, shadowColor = nil, align = 0)
+  $drawn_text.push(string)
+end
 
 class WindowSnagListTest < Minitest::Test
+
   def setup
-    # Do nothing
+    $player.snag_order.clear
+    $player.shadow_seen.clear
+    $drawn_text.clear
   end
 
   def teardown
-    # Do nothing
+    $player.snag_order.clear
+    $player.shadow_seen.clear
+    $drawn_text.clear
   end
 
-  def test
-    skip 'Not implemented'
+  def test_set_commands
+    window = WindowSnagList.new(0, 0, 640, 480)
+    assert_nil(window.species)
+
+    dummy_commands = [
+      {:species => :PIKACHU, :name => "Pikachu"},
+      {:species => :ELEKID, :name => "Elekid"},
+      {:species => :RAGINGBOLT, :name => "Raging Bolt"}
+    ]
+
+
+    $player.shadow_seen[:PIKACHU] = create_mock_snag_entry(false)
+    $player.shadow_seen[:ELEKID] = create_mock_snag_entry(true)
+
+    window.commands = dummy_commands
+
+    assert_equal(["Pikachu", "Elekid", "----------"], $drawn_text)
+  end
+
+  private
+
+  def create_mock_snag_entry(was_snagged)
+    mock = Minitest::Mock.new
+
+    if was_snagged
+      def mock.snagged
+        return true
+      end
+    else
+      def mock.snagged
+        return false
+      end
+    end
+
+    return mock
   end
 end
