67d45988019134308eceb5994a4525f85fcd2d2e
fcorso2016 <35567462+fcorso2016@users.noreply.github.com>
2024-01-05 21:20:12 -0500

    Moved signatures to live with their plugins

---
+++ Plugins/RetroUtils/lib/safety/no_such_element_exception.rb
@@ -0,0 +1,5 @@
+##
+# Thrown by various accessor methods to indicate that the element being requested does not exist.
+class NoSuchElementException < StandardError
+
+end
--- Plugins/RetroUtils/safety/no_such_element_exception.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class NoSuchElementException < StandardError
-
-end
